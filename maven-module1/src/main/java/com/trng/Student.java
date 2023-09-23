86c3abe5d0415c052a5e28a3261fa711b6295c4a
Khushi-meenia <khushimeenia1608@gmail.com>
2023-09-23 15:13:09 +0530

    Student file added

---
+++ maven-module1/src/main/java/com/trng/Student.java
@@ -0,0 +1,13 @@
+package com.trng;
+
+public class Student {
+    private String rollNo;
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+}
